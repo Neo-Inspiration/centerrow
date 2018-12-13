59075e98766b6dc2d917108c09b7a351fe646dd6
ESnark <settimeout1000@gmail.com>
2018-12-13 22:17:27 +0900

    no message

--- asset/js/centerrow.js
+++ asset/js/centerrow.js
@@ -84,5 +84,19 @@
             }
         });
 
+        // ---- html toggle ----
+        let policy = $('#cc-policy');
+        let detail = $('#cc-policy-detail');
+        if (policy.length > 0 && detail.length > 0) {
+            policy.click(() => {
+                let visible = detail.css('display') !== "none";
+
+                if (visible) {
+                    detail.slideUp();
+                } else {
+                    detail.slideDown();
+                }
+            });
+        }
     });
 })(jQuery)
